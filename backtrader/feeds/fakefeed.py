6d27bdde1e9675fda5b905af0b7fd134bd232550
verybadsoldier <vbs@springrts.de>
2020-02-24 10:19:34 +0100

    FakeFeed cleanups

--- backtrader/feeds/fakefeed.py
+++ backtrader/feeds/fakefeed.py
@@ -20,7 +20,6 @@ class FakeFeed(bt.DataBase):
         ('start_delay', 0),
         ('run_duration', datetime.timedelta(seconds=30)),  # only used when not backtest mode
         ('num_gen_bars', 10),  # number of bars to generate in backtest or backfill mode
-        ('use_orig_timeframe', True),
         ('live', True),
     )
 
@@ -32,7 +31,6 @@ class FakeFeed(bt.DataBase):
         self._cur_value = None
         self._current_comp = 0
         self._num_bars_delivered = 0
-        self._timeframe_in_effect = None
         self._compression_in_effect = None
         self._backfill_curtime = None
         self._tmoffset = datetime.timedelta(seconds=-0.5)  # configure offset cause we are sending slightly delayed ticked data (of course!)
@@ -43,8 +41,6 @@ class FakeFeed(bt.DataBase):
 
         self._start_ts = datetime.datetime.now()
         self._cur_value = self.p.starting_value
-        self._timeframe_in_effect = self.p.timeframe if self.p.use_orig_timeframe else self._timeframe
-        self._compression_in_effect = self.p.compression if self.p.use_orig_timeframe else self._compression
 
     def islive(self):
         return self.p.live
@@ -88,40 +84,22 @@ class FakeFeed(bt.DataBase):
         else:
             return self._load_bar(now)
 
-    # def _load_bar(self):
-    #     if self._last_delivered is None:
-    #         self._last_delivered = self._starttime = datetime.datetime.utcnow()
-    #         return None
-    #
-    #     if self._timeframe_in_effect == bt.TimeFrame.Ticks:
-    #         delta = self.p.tick_interval * self._compression_in_effect
-    #     elif self._timeframe_in_effect == bt.TimeFrame.Minutes:
-    #         delta = datetime.timedelta(minutes=self._compression_in_effect)
-    #     elif self._timeframe_in_effect == bt.TimeFrame.Days:
-    #         delta = datetime.timedelta(days=self._compression_in_effect)
-    #     else:
-    #         raise RuntimeError(f"{self._name} - Unsupported timeframe: {self.p.timeframe}")
-    #
-    #     self._last_delivered += delta
-    #     self._update_line(self._last_delivered, self._cur_value)
-    #     self._cur_value += 1
-    #     self._num_bars_delivered += 1
-
     def _load_bar(self, now, backfill=False):
-        if self._timeframe_in_effect == bt.TimeFrame.Ticks:
-            delta = self.p.tick_interval * self._compression_in_effect
-        elif self._timeframe_in_effect == bt.TimeFrame.Minutes:
-            delta = datetime.timedelta(minutes=self._compression_in_effect)
-        elif self._timeframe_in_effect == bt.TimeFrame.Days:
-            delta = datetime.timedelta(days=self._compression_in_effect)
+        tf, comp = (self.p.timeframe, self.p.compression) if not backfill else (self._timeframe, self._compression)
+        if tf == bt.TimeFrame.Ticks:
+            delta = self.p.tick_interval * comp
+        elif tf == bt.TimeFrame.Minutes:
+            delta = datetime.timedelta(minutes=comp)
+        elif tf == bt.TimeFrame.Days:
+            delta = datetime.timedelta(days=comp)
         else:
             raise RuntimeError(f"{self._name} - Unsupported timeframe: {self.p.timeframe}")
 
         if self._last_delivered is None:
             if backfill:
-                self._last_delivered = self._time_floored(now - delta * (self.p.num_gen_bars - 1))
+                self._last_delivered = self._time_floored(now - delta * (self.p.num_gen_bars - 1), tf)
             else:
-                self._last_delivered = self._time_floored(now)
+                self._last_delivered = self._time_floored(now, tf)
 
         self._last_delivered += delta
 
@@ -131,36 +109,40 @@ class FakeFeed(bt.DataBase):
         self._num_bars_delivered += 1
         return True
 
-    def _time_floored(self, now):
+    @staticmethod
+    def _time_floored(now, timeframe):
         t = now
-        if self._timeframe_in_effect in [bt.TimeFrame.Minutes, bt.TimeFrame.Ticks]:
+        if timeframe in [bt.TimeFrame.Minutes, bt.TimeFrame.Ticks]:
             t = t.replace(second=0, microsecond=0)
-        elif self._timeframe_in_effect == bt.TimeFrame.Days:
+        elif timeframe == bt.TimeFrame.Days:
             t = t.replace(minute=0, second=0, microsecond=0)
         return t
 
     def _load_live(self, now):
+        tf = self.p.timeframe
+        comp = self.p.compression
+
         if self._last_delivered is None:
             # first run, fill last_delivered
-            self._last_delivered = self._time_floored(now)
+            self._last_delivered = self._time_floored(now, tf)
 
-        if self._timeframe_in_effect == bt.TimeFrame.Ticks:
+        if tf == bt.TimeFrame.Ticks:
             if now - self._last_delivered < self.p.tick_interval:
                 return None
             self._last_delivered += self.p.tick_interval
         else:
-            if self._timeframe_in_effect == bt.TimeFrame.Minutes:
+            if tf == bt.TimeFrame.Minutes:
                 if now.minute == self._last_delivered.minute:
                     return None
                 self._last_delivered += datetime.timedelta(minutes=1)
-            elif self._timeframe_in_effect == bt.TimeFrame.Days:
+            elif tf == bt.TimeFrame.Days:
                 if now.day == self._last_delivered.day:
                     return None
                 self._last_delivered += datetime.timedelta(days=1)
 
         self._current_comp += 1
 
-        if self._current_comp == self._compression_in_effect:  # do not use self._compression as it is modified by resampler already
+        if self._current_comp == comp:  # do not use self._compression as it is modified by resampler already
             self._current_comp = 0
 
             self._update_line(self._last_delivered, self._cur_value)
