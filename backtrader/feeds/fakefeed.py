605542b65f4e05fb55b7af35ee82ded413680e76
verybadsoldier <vbs@springrts.de>
2020-03-05 10:52:23 +0100

    FakeFeed: fix bar generation

--- backtrader/feeds/fakefeed.py
+++ backtrader/feeds/fakefeed.py
@@ -1,3 +1,4 @@
+import math
 import datetime
 import logging
 import time
@@ -48,17 +49,33 @@ class FakeFeed(bt.DataBase):
     def _update_line(self, dt, value):
         _logger.debug(f"{self._name} - Updating line - Bar Time: {dt} - Value: {value}")
 
-        #if dt.hour == 18:
-        #    dt = dt.replace(hour=17, minute=35)
         self.lines.datetime[0] = bt.date2num(dt)
         self.lines.volume[0] = 0.0
         self.lines.openinterest[0] = 0.0
 
         # Put the prices into the bar
-        self.lines.open[0] = value
-        self.lines.high[0] = value
-        self.lines.low[0] = value
+        if math.isnan(self.lines.open[0]):
+            self.lines.open[0] = value
+        if math.isnan(self.lines.high[0]) or value > self.lines.high[0]:
+            self.lines.high[0] = value
+        if math.isnan(self.lines.low[0]) or value < self.lines.low[0]:
+            self.lines.low[0] = value
         self.lines.close[0] = value
+        self.lines.volume[0] = 0.0
+        self.lines.openinterest[0] = 0.0
+
+    def _update_bar(self, dt, vopen, vlow, vhigh, vclose):
+        _logger.debug(f"{self._name} - Updating bar - Bar Time: {dt} - Value: {vclose}")
+
+        self.lines.datetime[0] = bt.date2num(dt)
+        self.lines.volume[0] = 0.0
+        self.lines.openinterest[0] = 0.0
+
+        # Put the prices into the bar
+        self.lines.open[0] = vopen
+        self.lines.high[0] = vhigh
+        self.lines.low[0] = vlow
+        self.lines.close[0] = vclose
         self.lines.volume[0] = 0.0
         self.lines.openinterest[0] = 0.0
 
@@ -85,7 +102,7 @@ class FakeFeed(bt.DataBase):
             return self._load_bar(now)
 
     def _load_bar(self, now, backfill=False):
-        tf, comp = self._timeframe, self._compression
+        tf, comp = (self.p.timeframe, self.p.compression) if not backfill else (self._timeframe, self._compression)
         if tf == bt.TimeFrame.Ticks:
             delta = self.p.tick_interval * comp
         elif tf == bt.TimeFrame.Seconds:
@@ -106,8 +123,14 @@ class FakeFeed(bt.DataBase):
         self._last_delivered += delta
 
         _logger.info(f"{self._name} - Loading bar: {self._backfill_curtime}")
-        self._update_line(self._last_delivered, self._cur_value)
-        self._cur_value += 1
+
+        if backfill:
+            self._update_bar(self._last_delivered, self._cur_value, self._cur_value, self._cur_value + comp, self._cur_value + comp)
+            self._cur_value += comp
+        else:
+            self._update_line(self._last_delivered, self._cur_value)
+            self._cur_value += 1
+
         self._num_bars_delivered += 1
         return True
 
@@ -116,7 +139,7 @@ class FakeFeed(bt.DataBase):
         t = now
         if timeframe in [bt.TimeFrame.Seconds, bt.TimeFrame.Ticks]:
             t = t.replace(microsecond=0)
-        elif timeframe in bt.TimeFrame.Minutes:  # Ticks get also floored to last full second
+        elif timeframe == bt.TimeFrame.Minutes:  # Ticks get also floored to last full second
             t = t.replace(second=0, microsecond=0)
         elif timeframe == bt.TimeFrame.Days:
             t = t.replace(hour=0, minute=0, second=0, microsecond=0)
